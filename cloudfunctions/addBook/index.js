4662404cb8362ca83f9b17cdddc6aa1ee548c030
yunyiGG <zl0542@foxmail.com>
2020-09-05 17:05:01 +0800

    增加addbook 的云函数

--- cloudfunctions/addBook/index.js
+++ cloudfunctions/addBook/index.js
@@ -1,16 +1,43 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+const addBook = async ({ bookName, amount, date }) => {
+  const wxContext = cloud.getWXContext()
+  const db = cloud.database();
+  const openid = wxContext.OPENID;
+
+  return await db.collection('accountBook').add({
+    data: {
+      bookName,
+      initAmount: amount,
+      initDate: date,
+      currency: 'CNY',
+      createTime: new Date(),
+      isDeleted: false,
+      openid
+    }
+  })
+}
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  try {
+    const re = await addBook(event);
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+    return {
+      msg:'success'
+    }
+
+  } catch (err) {
+    return {
+      event,
+      openid: wxContext.OPENID,
+      appid: wxContext.APPID,
+      unionid: wxContext.UNIONID,
+    }
   }
 }
